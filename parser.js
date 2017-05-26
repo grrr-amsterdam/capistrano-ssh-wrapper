e863bdb8a5451be36f071bdb8d72aa5f6aab3c37
David Spreekmeester <david@grrr.nl>
2017-05-26 14:36:30 +0200

    Added more flexibility to Cap parsing

--- parser.js
+++ parser.js
@@ -20,31 +20,64 @@ var parser = module.exports = {
 
     _parseSshConfigLine: function(line) {
         if (line.startsWith('server')) {
-            var userhost = /server ['"]+(.*)@(.*)['"]+/g;
-            var match = userhost.exec(line);
-            if (match.length < 2) {
-                var e = 'Could not find SSH config.'
-                error.exit(e)
+            var parsed = parser._parseSshServerLine(line)
+            if (!parsed) {
+                parsed = parser._parseSshServerSeparateUserLine(line)
             }
 
-            return {
-                user: match[1],
-                host: match[2]
-            }
+            return parsed
         }
 
         if (line.startsWith('set :deploy_to')) {
-            var dir = /:deploy_to, ['"]+(.*)['"]+/g;
-            var match = dir.exec(line);
-            if (match.length < 1) {
-                var e = 'Could not find SSH deploy dir config.'
-                error.exit(e)
-            }
-
-            return {
-                dir: match[1]
-            }
+            return parser._parseSshDeployLine(line)
         }
     },
+
+    _parseSshServerLine: function(line) {
+        var userhost = /server ['"]+(.*)@(.*)['"]+/g;
+        var match = userhost.exec(line);
+
+        if (
+            !('length' in match) ||
+            match.length < 2
+        ) {
+            return
+        }
+
+        return {
+            user: match[1],
+            host: match[2]
+        }
+    },
+
+    _parseSshServerSeparateUserLine: function(line) {
+        var userhost = /server ['"]+(.*)['"]+user: ['"]+(.*)['"]/g;
+        var match = userhost.exec(line);
+
+        if (
+            !('length' in match) ||
+            match.length < 2
+        ) {
+            return
+        }
+
+        return {
+            host: match[1],
+            user: match[2]
+        }
+    },
+
+    _parseSshDeployLine: function(line) {
+        var dir = /:deploy_to, ['"]+(.*)['"]+/g;
+        var match = dir.exec(line);
+        if (match.length < 1) {
+            var e = 'Could not find SSH deploy dir config.'
+            error.exit(e)
+        }
+
+        return {
+            dir: match[1]
+        }
+    }
 };
 
