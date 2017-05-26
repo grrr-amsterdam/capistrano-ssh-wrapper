08aaac1109c8972c0f432d0ff03c7c2ca9329b96
David Spreekmeester <david@grrr.nl>
2017-05-26 13:38:15 +0200

    Added rejections for missing arguments

--- wrapper.js
+++ wrapper.js
@@ -12,6 +12,14 @@ var wrapper = module.exports = {
      * @return Promise
      */
     exec: function(target, command) {
+        if (!target) {
+            return Promise.reject('No target environment name given.')
+        }
+
+        if (!command) {
+            return Promise.reject('No command given to execute.')
+        }
+
         var ssh_config = config.findSshConfig(target)
         if (!ssh_config) {
             return Promise.reject('No Capistrano SSH config was found.')
